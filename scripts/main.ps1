9b423ca756bc6a29c8091189702a09438b8b28c0
Marcus Jochimsen <jochimsen.marcus@googlemail.com>
2014-12-08 13:25:35 +0100

    Update main.ps1

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -4,13 +4,21 @@
 ## Funktion um die Verbindung aufzubauen
 connect
 
+## User anlegen
+$global:Query = 'SELECT * FROM benutzer'
+$mysqlresults = Get-SqlDataTable $Query
+
+ForEach ($result in $mysqlresults){
+	$secure_string_pwd = convertto-securestring $($result.pass) -asplaintext -force
+	New-ADUser -Name $($result.login) -AccountNotDelegated $false -AuthType "Negotiate" -CannotChangePassword $false -ChangePasswordAtLogon $true -Company $($result.company) -Department $($result.abteilung) -Description $($result.description) -EmailAddress $($result.email) -EmployeeID $($result.id) -GivenName $($result.vorname) -MobilePhone $($result.mphone) -Office $($result.office) -OfficePhone $($result.ophone) -PasswordNeverExpires $false -PasswordNotRequired $false -Surname $($result.nachname) -Title $($result.description) -TrustedForDelegation $true -AccountPassword $secure_string_pwd -Enabled $true -ProfilePath "\\dc\profiles$\%username%"
+
+}
+
 ## OUs anlegen
 $global:Query = 'SELECT * FROM ou'
 $mysqlresults = Get-SqlDataTable $Query
 
 ForEach ($result in $mysqlresults){
-	## echo $($result.name)
-	## echo $($result.description)
 	New-ADOrganizationalUnit -Name $($result.name) -Description $($result.description) -DisplayName $($result.name) -ProtectedFromAccidentalDeletion $false
 
 }
