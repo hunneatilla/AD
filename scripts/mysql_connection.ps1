661bf08a45498cb2997981f44bb1b5899becae39
Marcus Jochimsen <jochimsen.marcus@googlemail.com>
2014-12-08 11:42:50 +0100

    Update and rename mySQL Connection to mysql_connection.ps1

--- scripts/mysql_connection.ps1
+++ scripts/mysql_connection.ps1
@@ -6,6 +6,8 @@
 #####################################################################################################
 #####################################################################################################
 
+function connect {
+
 ## MYSQL Connection
 ## This requires mysql connector net
 
@@ -60,16 +62,4 @@ $mysqltest = Get-SqlDataTable 'SHOW STATUS'
 #####################################################################################################
 #####################################################################################################
 
-## OUs anlegen
-$global:Query = 'SELECT * FROM ou'
-$mysqlresults = Get-SqlDataTable $Query
-
-ForEach ($result in $mysqlresults){
-	## echo $($result.name)
-	## echo $($result.description)
-	New-ADOrganizationalUnit -Name $($result.name) -Description $($result.description) -DisplayName $($result.name) -ProtectedFromAccidentalDeletion $false
-
 }
-
-## Gruppen anlegen
-
