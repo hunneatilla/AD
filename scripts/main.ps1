542467d8422446d2924ebd4956169f5d6418965a
Marcus Jochimsen <jochimsen.marcus@googlemail.com>
2014-12-09 10:27:46 +0100

    Update main.ps1

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -19,7 +19,7 @@ $global:Query = 'SELECT * FROM rechner'
 $mysqlresults = Get-SqlDataTable $Query
 
 ForEach ($result in $mysqlresults){
-	[string]$dnsname = $($result.name)+".smart-in-hamburg.org"
+	[string]$dnsname = ""+$($result.name)+".smart-in-hamburg.org"
 	New-ADComputer -Description $($result.description) -DisplayName $($result.displayname) -DNSHostName $dnsname -Name $($result.name) -ManagedBy $($result.manage) -OperatingSystem $($result.os)
 }
 
