3b2ae9e1c751c1a62a4a698044a9d8609cce98fe
Marcus Jochimsen <jochimsen.marcus@googlemail.com>
2014-12-09 13:19:39 +0100

    Update reset_password.ps1

--- scripts/reset_password.ps1
+++ scripts/reset_password.ps1
@@ -7,13 +7,12 @@ while(($eingabe = Read-Host -Prompt "Wählen Sie bitte eine Raumnummer -  z.B 4:
 		3 {$zahlEins=3}
 		4 {$zahlEins=4}
 		5 {$zahlEins=5}
-        6 {$zahlEins=6}
+        	6 {$zahlEins=6}
 		Q {"Ende"}
 		default {"Ungültige Eingabe"}
    }
-}
-
-for ($zahlZwei=1;$zahlZwei -ge 12;$zahlZwei++){
-Set-ADAccountPassword -Identity FLee -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "Changme1!" -Force)
-#Set-ADAccountPassword -Identity "s" + $zahlEins + ".p"+ $zahlZwei -OldPassword (ConvertTo-SecureString -AsPlainText "p@ssw0rd" -Force) -NewPassword (ConvertTo-SecureString -AsPlainText "qwert@12345" -Force)
+	for ($zahlZwei=1;$zahlZwei -ge 12;$zahlZwei++){
+	Set-ADAccountPassword -Identity FLee -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "Changme1!" -Force)
+	#Set-ADAccountPassword -Identity "s" + $zahlEins + ".p"+ $zahlZwei -OldPassword (ConvertTo-SecureString -AsPlainText "p@ssw0rd" -Force) -NewPassword (ConvertTo-SecureString -AsPlainText "qwert@12345" -Force)
+	}
 }
