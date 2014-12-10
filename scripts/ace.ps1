59d291ce912c1dcdd345dfc2487ae57666706492
Marcus Jochimsen <jochimsen.marcus@googlemail.com>
2014-12-10 13:13:07 +0100

    Create ace.ps1

---
+++ scripts/ace.ps1
@@ -0,0 +1,16 @@
+$DirectoryPath="c:\temp\homes\homeuser001"
+# $IdentityRef = "DOM1\Karl_Napf"  #User oder Group
+$IdentityRef = Get-ADObject -Identity Benutzer
+
+$ACL = Get-ACL $DirectoryPath
+$ACEs=(Get-Acl $DirectoryPath).Access | where {$_.IdentityReference -eq $IdentityRef}
+$ACEs | foreach{
+    try{
+    $null=$ACL.RemoveAccessRule($_)
+    Set-ACL $DirectoryPath $ACL
+    "ACE für $IdentityRef erfolgreich entfernt"
+    }
+    catch{
+    #keine ACE mehr vorhanden
+    }
+ }
