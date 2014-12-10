3b1b85f434340838bfe08192ee18f070921622f4
Marcus Jochimsen <jochimsen.marcus@googlemail.com>
2014-12-10 11:13:13 +0100

    Update main.ps1

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -43,11 +43,11 @@ $global:Query = 'SELECT * FROM benutzer'
 $mysqlresults = Get-SqlDataTable $Query
 
 ForEach ($result in $mysqlresults){
-	$Drive = "\\dc\Homedrive.$($result.login)"
+	$Drive = "\\dc\$($result.login)"
 	$secure_string_pwd = convertto-securestring $($result.password) -asplaintext -force
 	New-ADUser -Name $($result.login) -AccountNotDelegated $false -AuthType "Negotiate" -CannotChangePassword $false -ChangePasswordAtLogon $true -Company $($result.company) -Department $($result.abteilung) -Description $($result.description) -EmailAddress $($result.email) -EmployeeID $($result.id) -GivenName $($result.vorname) -MobilePhone $($result.mphone) -Office $($result.office) -OfficePhone $($result.ophone) -PasswordNeverExpires $false -PasswordNotRequired $false -Surname $($result.nachname) -Title $($result.description) -TrustedForDelegation $true -AccountPassword $secure_string_pwd -Enabled $true -ProfilePath "\\dc\profile$\%username%" -HomeDrive "Z:" -HomeDirectory $Drive
 	New-Item -Path "C:\smart\home\" -Name $($result.login) -ItemType directory
-	New-SmbShare -Name Homedrive.$($result.login) -Path C:\smart\home\$($result.login) -Description HomeShare
+	New-SmbShare -Name $($result.login) -Path C:\smart\home\$($result.login) -Description HomeShare
 	write-host "Benutzer $($result.login) wurde erstellt."
 }
 
