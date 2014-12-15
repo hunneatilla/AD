31616c4ef9560fbe817e5b45583b06b9caa96df3
Marcus Jochimsen <jochimsen.marcus@googlemail.com>
2014-12-15 10:30:27 +0100

    Update main.ps1

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,14 +1,23 @@
 ## Datei mit der connect-Funktion
 #. $(Get-Location).Path\mysql_connection.ps1
-. C:\Skripte\mysql_connection.ps1 # connect ()
-. C:\Skripte\alc.ps1 # set-acl ($DirectoryPath, $IdentityRef, $rights)
-. C:\Skripte\folder.ps1 # new-folder ()
-. C:\Skripte\share.ps1 # set-share ()
-. C:\Skripte\user.ps1 # new-user ()
-. C:\Skripte\pc.ps1 # new-pc ()
-. C:\Skripte\ou.ps1 # new-ou () # verschachtel-ou ()
-. C:\Skripte\group.ps1 # new-group ()
-. C:\Skripte\transport.ps1 # transport-user-in-ou () # transport-pc-in-ou () # transport-user-in-group ()
+. C:\Skripte\mysql_connection.ps1 
+# connect ()
+. C:\Skripte\alc.ps1 
+# set-acl ($DirectoryPath, $IdentityRef, $rights)
+. C:\Skripte\folder.ps1 
+# new-folder ()
+. C:\Skripte\share.ps1 
+# set-share ()
+. C:\Skripte\user.ps1 
+# new-user ()
+. C:\Skripte\pc.ps1 
+# new-pc ()
+. C:\Skripte\ou.ps1 
+# new-ou () # verschachtel-ou ()
+. C:\Skripte\group.ps1 
+# new-group ()
+. C:\Skripte\transport.ps1 
+# transport-user-in-ou () # transport-pc-in-ou () # transport-user-in-group ()
 
 ## mySQL Verbindung aufbauen
 connect
