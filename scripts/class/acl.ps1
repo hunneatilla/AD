afda70a8f5dd2dc8b4e359e71b43fdc5664005eb
Marcus Jochimsen <jochimsen.marcus@googlemail.com>
2014-12-15 14:53:36 +0100

    Update acl.ps1

--- scripts/class/acl.ps1
+++ scripts/class/acl.ps1
@@ -31,8 +31,9 @@ Set-ACL $DirectoryPath $ACL
 function setacl2 ($DirectoryPath, $IdentityRef, $rights) 
 {
     $ACL = Get-Acl $DirectoryPath
-    $ACE = New-Object System.Security.AccessControl.FileSystemAccessRule ($IdentityRef,$rights, "ContainerInherit, ObjectInherit", "Inheritonly", "Allow")
+    $ACE = New-Object System.Security.AccessControl.FileSystemAccessRule ("$IdentityRef","$rights", "ContainerInherit, ObjectInherit", "Inheritonly", "Allow")
     $ACL.AddAccessRule($ACE)
+    
     Set-Acl $DirectoryPath $ACL
 }
 
